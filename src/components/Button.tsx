5deb1b14390cebabea584961a89498a3ab35dfd8
eytan elfassi <eytan.elfassi@epita.fr>
2017-07-10 00:12:57 +0200

    adding commentary

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-
+// This fonction make the color change on the button on the first click
 function ChangeStyle() {
        var elem = document.getElementById("Button1");
        if (elem == null)
@@ -9,9 +9,11 @@ function ChangeStyle() {
        
 }
 
+//Main Class
 export class Button extends React.Component {
     render() {
 
+        //Style sheet of the Button
         var buttonStyle = {
             fontSize: 50,
             background: "#ffb516",
@@ -24,10 +26,12 @@ export class Button extends React.Component {
             margin: "auto"  
         }
   
-
+        //Set the Background color
         document.body.style.backgroundColor =  "#25353f";
         document.body.style.position = "relative";
         
+
+        //Try to make the button in center
         /*var centerDiv = {
             position: "absolute" as "absolute",
             top: "50%",
@@ -35,6 +39,8 @@ export class Button extends React.Component {
             margin: "auto"      
         }*/
 
+
+        //Test on linearGradient
       /*  function degraded() {
             <svg width="15" height="71" fill="url(#Gradient-1)">
                 <defs>
@@ -46,7 +52,7 @@ export class Button extends React.Component {
             </svg>
         }*/
 
-        
+        //Create the button
         return (   
             <div /*style={centerDiv}*/>           
                      <button id="Button1" style={buttonStyle}
